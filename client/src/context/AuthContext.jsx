8a8ca487280444d8621f9be7689ebb9689184e62
leandrogrzn <hekoej@gmail.com>
2024-06-12 17:14:01 -0500

    login

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState, useContext, useSyncExternalStore } from "react"
-import { registerRequest } from '../api/auth'
+import { createContext, useState, useContext, useEffect } from "react"
+import { registerRequest, loginRequest } from '../api/auth'
 
 export const AuthContext = createContext()
 
@@ -27,8 +27,29 @@ export const AuthProvider = ({ children }) => {
         }
     }
 
+    const signin = async (user) => {
+        try {
+            const res = await loginRequest(user)
+            console.log(res)
+        } catch (error) {
+            if(Array.isArray(error.response.data)) {
+                return setErrors(error.response.data)
+            }
+            setErrors([error.response.data.message])
+        }
+    }
+
+    useEffect(() => {
+        if(errors.length > 0) {
+            const timer = setTimeout(() => {
+                setErrors([])
+            }, 5000)
+            return () => clearTimeout(timer)
+        }
+    }, [errors])
+
     return(
-        <AuthContext.Provider value={{signup, user, isAuthenticated, errors}}>
+        <AuthContext.Provider value={{signup, user, isAuthenticated, errors, signin}}>
             { children }
         </AuthContext.Provider>
     )
